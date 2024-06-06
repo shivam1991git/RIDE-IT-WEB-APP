17cd6381b777d17072123f1297f9a3bf3059d616
Shivam Singh <148255149+shivam1991git@users.noreply.github.com>
2024-06-06 13:57:49 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectDB(){
-    mongoose.connect('mongodb+srv://shivam:sksthakur@cluster0.r9cvxfe.mongodb.net/RIDO', 
+    mongoose.connect('mongodb+srv://shivaaay:<password>@cluster0.r9cvxfe.mongodb.net/', 
     {  
         useUnifiedTopology: true,
         useNewUrlParser: true
